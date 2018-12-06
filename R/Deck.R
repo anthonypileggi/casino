328e1aee6799a9ac8d5d7d47ae6d1bdf82b27d3e
anthonypileggi <anthony@ifixit.com>
2018-12-06 09:13:39 -0500

    Deck class: add examples

--- R/Deck.R
+++ R/Deck.R
@@ -1,5 +1,27 @@
 #' Deck R6 Class
 #' @importFrom magrittr "%>%"
+#' @examples
+#' # create a new deck
+#' x <- Deck$new()
+#' x
+#'
+#' # draw a card
+#' x$draw(1)
+#' x
+#'
+#' # draw 10 cards
+#' x$draw(10)
+#'
+#' # check how many cards are left
+#' x$cards_left()
+#'
+#' # reset the deck
+#' x$shuffle()
+#' x
+#'
+#' # create a deck composed of 5 decks
+#' x <- Deck$new(decks = 5)
+#' x
 #' @export
 Deck <- R6::R6Class("Deck",
   public = list(
