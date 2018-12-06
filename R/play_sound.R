79f824260f0d87e5775622e1a709b4e5a1955976
anthonypileggi <anthony@ifixit.com>
2018-12-06 14:02:30 -0500

    fix typo; update pkgdown website

--- R/play_sound.R
+++ R/play_sound.R
@@ -1,6 +1,6 @@
 
 #' Play a sound (if possible)
-#' @param sound character string or number specificying sound (see \code{\link[beepr]{beep}})
+#' @param sound character string or number specifying the sound (see \code{\link[beepr]{beep}})
 #' @note requires the `beepr` package
 play_sound <- function(sound = "fanfare") {
   if ("beepr" %in% utils::installed.packages()[, 1])
